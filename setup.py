7e0ca1386c1390a6cc5813691e94aa16d8d5882f
Shunsuke Ohashi <shunsuke@indeed.com>
2015-10-11 23:37:54 +0900

    added install_reuires

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(name='guacamole',
       version='1.0',
       packages=['mirt'],
-      py_modules=['start_mirt_pipeline'])
+      py_modules=['start_mirt_pipeline'],
+      install_requires=['numpy', 'scipy', 'affinity', 'matplotlib'])
 
 
